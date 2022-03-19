746e429bd49602ae641cfdcc6c8b2dd7ca06e0de
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-03-19 19:49:39 +0300

    Add the contact list class variable

--- contact.py
+++ contact.py
@@ -3,6 +3,8 @@ class Contact:
     Class that generates new instances of contacts
     """
 
+    contact_list = [] #Empty contact list
+
     def __init__(self, first_name, last_name, phone_number, email):
         """
         __init__ method that helps us define propertes for our objects.
