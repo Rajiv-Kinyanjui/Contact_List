a9e4f261d63df106c88e369b7401dbe4c600b5f3
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-03-20 14:27:20 +0300

    Add the unittest module imports and the subclass test contact that inherits from the parent class unittest.TestCase

---
+++ contact_test.py
@@ -0,0 +1,10 @@
+import unittest #Importing the unittest module
+from contact import Contact
+
+class TestContact(unittest.TestCase):
+    '''
+    Test class that defines test cases for the contact class behaviours
+
+    Args:
+        unittest.Testcase: TestCase class that helps test cases
+    '''
