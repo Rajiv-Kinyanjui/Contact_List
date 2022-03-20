378d1b4c0729783b5e47b6fc779e689279bfff1b
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-03-20 21:05:49 +0300

    Add the save_contact function to append new contacts to the contact_list list

--- contact.py
+++ contact.py
@@ -19,3 +19,10 @@ class Contact:
         self.last_name = last_name
         self.phone_number = phone_number
         self.email = email
+    
+
+    def save_contact(self):
+        '''
+        save_contact saves contact objects into contact_list
+        '''
+        Contact.contact_list.append(self)
