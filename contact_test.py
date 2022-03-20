c5eb77f5132040b1ed696cffba0332f111325893
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-03-20 14:44:24 +0300

    Add the set upand test init methods to conduct initial tests creating a new instance of the contact everytime and checking if the objects are instantiated correctly

--- contact_test.py
+++ contact_test.py
@@ -8,3 +8,22 @@ class TestContact(unittest.TestCase):
     Args:
         unittest.Testcase: TestCase class that helps test cases
     '''
+    #First Test
+    def setUp(self):
+        '''
+        Set up method runs before each test case
+        '''
+        self.new_contact = Contact("James","Muriuki", "0712345678", "james@ms.com") #Create contact object
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object in initiatted proberly
+        '''
+        self.assertEqual(self.new_contact.first_name,"James")
+        self.assertEqual(self.new_contact.last_name, "Muriuki")
+        self.assertEqual(self.new_contact.phone_number, "0712345678")
+        self.assertEqual(self.new_contact.email, "james@ms.com")
+
+
+if __name__ == '__main__':
+    unittest.main
