ac774e8460e1028efde2f7a94983eed778297148
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-03-24 18:15:42 +0300

    Add comments to the functions that describe them

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.6
 from contact import Contact
 
+#Creating a contact
 def create_contact(fname, lname, phone, email):
     '''
     function to create a new contact
@@ -9,30 +10,40 @@ def create_contact(fname, lname, phone, email):
 
     return new_contact
 
+
+#Save contacts
 def save_contacts(contact):
     '''
     function to save contact
     '''
     contact.save_contact()
 
+
+#Delete contact
 def del_contact(contact):
     '''
     function to delete contacts
     '''
     contact.delete_contact()
 
+
+#Finding a contact
 def find_contact(number):
     '''
     Functions that finds a contact by number and returns the contact
     '''
     return Contact.find_by_number(number)
 
+
+#Check if contact exists
 def check_existing_contacts(number):
     '''
     function that checks if a contact exists with that number and returns a boolean
     '''
     return Contact.contact_exist(number)
 
+
+#Displaying all contacts
 def display_contact():
     '''
     Function that returns all saved contacts
