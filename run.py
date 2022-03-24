54947c85b8d64790389142a96e827b55a8492674
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-03-24 18:03:33 +0300

    Add the create new contact function

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3.6
+from contact import Contact
+
+def create_contact(fname, lname, phone, email):
+    '''
+    function to create a new contact
+    '''
+    new_contact = Contact(fname, lname, phone, email)
+    
+    return new_contact
