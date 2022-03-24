7170429e367e4df7d163967e47cf6b4f0ddf5f7f
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-03-24 18:05:04 +0300

    Add the save contact function

--- run.py
+++ run.py
@@ -6,5 +6,12 @@ def create_contact(fname, lname, phone, email):
     function to create a new contact
     '''
     new_contact = Contact(fname, lname, phone, email)
-    
+
     return new_contact
+
+def save_contacts(contact):
+    '''
+    function to save contact
+    '''
+    contact.save_contact()
+
